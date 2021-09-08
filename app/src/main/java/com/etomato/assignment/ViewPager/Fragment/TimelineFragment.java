1be54e275f15122bde609a708355b8ff34589224
etomato <ksjmgrkks@naver.com>
2021-09-08 18:42:34 +0900

    2021 09 08 커밋 2

--- app/src/main/java/com/etomato/assignment/ViewPager/Fragment/TimelineFragment.java
+++ app/src/main/java/com/etomato/assignment/ViewPager/Fragment/TimelineFragment.java
@@ -1,11 +1,15 @@
 package com.etomato.assignment.ViewPager.Fragment;
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.widget.Button;
 import android.widget.Toast;
 
 import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -21,13 +25,19 @@ import android.view.autofill.AutofillValue;
 import com.etomato.assignment.Main.Model;
 import com.etomato.assignment.Main.MyAdapter;
 import com.etomato.assignment.Main.ViewType;
+import com.etomato.assignment.MainActivity;
 import com.etomato.assignment.R;
 import com.github.vipulasri.timelineview.TimelineView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -37,12 +47,16 @@ import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 public class TimelineFragment extends Fragment {
     //객체 초기화 설정
     ArrayList<TimeLineModel> dataList;
     TimeLineAdapter myAdapter;
     DatabaseReference mDatabase;
+    Query myMostViewedPostsQuery;
+    Button buttonSort;
+    int filter=1;
 
     public TimelineFragment() {
         // Required empty public constructor
@@ -60,21 +74,34 @@ public class TimelineFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_timeline, container, false);
 
+        RecyclerView recyclerViewTimeLine = (RecyclerView) view.findViewById(R.id.recyclerView_timeline);
+        buttonSort = (Button) view.findViewById(R.id.button_sort) ;
+
+        buttonSort.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              showDialog();
+          }
+        });
+
         //리사이클러뷰 레이아웃매니저, 어댑터 설정
         LinearLayoutManager manager = new LinearLayoutManager(getActivity());
         dataList = new ArrayList<>();
         myAdapter = new TimeLineAdapter(dataList);
 
-        RecyclerView recyclerViewTimeLine = (RecyclerView) view.findViewById(R.id.recyclerView_timeline);
-
         recyclerViewTimeLine.setLayoutManager(manager);
         recyclerViewTimeLine.setAdapter(myAdapter);
 
-        //
-
-
         //db 인스턴스화
-        mDatabase = FirebaseDatabase.getInstance().getReference().child("timeline");
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        myMostViewedPostsQuery =  mDatabase.child("timeline").orderByChild("title");
+//        if(filter == 2){
+//            myMostViewedPostsQuery =  mDatabase.child("timeline").orderByChild("title");
+//        }else if (filter == 3){
+//            myMostViewedPostsQuery =  mDatabase.child("timeline").orderByChild("contents");
+//        }else{
+//            myMostViewedPostsQuery =  mDatabase.child("timeline");
+//        }
         //db 읽기 (실시간 반영)
         dbRead();
 
@@ -83,7 +110,7 @@ public class TimelineFragment extends Fragment {
 
     public void dbRead(){
         // 데이터베이스 읽기
-        mDatabase.addValueEventListener(new ValueEventListener() {
+        myMostViewedPostsQuery.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 if(dataSnapshot.exists()){
@@ -106,5 +133,32 @@ public class TimelineFragment extends Fragment {
                 Toast.makeText(getActivity(),""+error+"",Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    void showDialog(){
+        final List<String> ListItems = new ArrayList<>();
+        ListItems.add("최신순");
+        ListItems.add("제목순");
+        ListItems.add("내용순");
+        final CharSequence[] items =  ListItems.toArray(new String[ ListItems.size()]);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("타임라인 정렬");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int pos) {
+                String selectedText = items[pos].toString();
+                if(selectedText.equals("최신순")){
+                    buttonSort.setText("최신순");
+                    filter = 1;
+                }else if(selectedText.equals("제목순")){
+                    buttonSort.setText("제목순");
+                    filter = 2;
+                }else{
+                    buttonSort.setText("내용순");
+                    filter = 3;
+                }
+            }
+        });
+        builder.show();
     }
 }
